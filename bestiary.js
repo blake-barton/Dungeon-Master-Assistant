ec6eaf82e9a4b42c0371a088f8661a01abc4dced
Blake Barton <blake.barton@tamu.edu>
2020-11-23 10:47:59 -0600

    Bestiary is now duplicate safe (except for search)

--- bestiary.js
+++ bestiary.js
@@ -1,6 +1,47 @@
+function removeDupMarks(list)
+{
+    var regex = /\(\d+\)/gm;
+    const subst = ``;
+
+    for (var i = 0; i < list.length; i++)
+    {
+        var str = list[i];
+        list[i] = str.replace(regex, subst);
+    }
+
+    return list;
+}
+
+function findDuplicates(name)
+{
+    var dupCount = 0;
+    var list = removeDupMarks(loadNamesIntoArray());
+
+    for (var i = 0; i < list.length; i++)
+    {
+        if (name === list[i])
+        {
+            dupCount++;
+        }
+    }
+    
+    return dupCount;
+}
+
 function createMonsterJSON(filename, monsterJSON, monsterName)
 {
     var folderName = "DMAssistant";
+
+    // handle if this name is a duplicate
+    var dupCount = findDuplicates(monsterName);
+
+    // add number if duplicate
+    if (dupCount > 0)
+    {
+        filename = filename.replace(".json", "");
+        filename += "(" + dupCount + ").json";
+        monsterName += "(" + dupCount + ")";
+    }
 
     // save monster json
     var folderList = DriveApp.getFoldersByName(folderName);
