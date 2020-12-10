283eed35d42f58032f290bc3c7069662d04f0d36
Blake Barton <blake.barton@tamu.edu>
2020-12-09 22:52:44 -0600

    Player list display working
    Still needs removal, search, collapse all

--- gs/players.js
+++ gs/players.js
@@ -31,3 +31,52 @@ function appendPlayerJSON(newPlayerJSON)
         }
     }
 }
+
+function sortByPlayerName(a, b)
+{
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+
+    if (nameA < nameB)
+    {
+        return -1;
+    }
+    else if (nameA > nameB)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+function generatePlayerObjectArray()
+{
+    // access json
+    var folderName = "DMAssistantPlayers";
+    var filename = "playerList.json";
+
+    var folderList = DriveApp.getFoldersByName(folderName);
+    if (folderList.hasNext())
+    {
+        var folder = folderList.next();
+
+        // pull players.json down as an array of objects
+        var fileList = folder.getFilesByName(filename);
+        while (fileList.hasNext())
+        {
+            // found matching file, loading json
+            var file = fileList.next();
+            var jsonText = file.getBlob().getDataAsString();
+            
+            // get as javascript object
+            var playerArray = JSON.parse(jsonText);
+
+            // sort the array
+            playerArray.sort(sortByPlayerName);
+
+            return playerArray;
+        }
+    }
+}
